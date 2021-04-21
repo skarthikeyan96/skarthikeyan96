1b1048136b9502007d327a8b31f8bd8d007cdcff
karthikeyan <karthikeyan@bangthetable.com>
2021-04-21 17:34:26 +0530

    feat: rendering the readme with mustache

---
+++ index.js
@@ -0,0 +1,34 @@
+// index.js
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './main.mustache';
+/**
+  * DATA is the object that contains all
+  * the data to be provided to Mustache
+  * Notice the "name" and "date" property.
+*/
+let DATA = {
+  banner: 'https://user-images.githubusercontent.com/23126394/114269492-08f9fb00-9a25-11eb-89e0-a46912340a0b.png',
+  name: 'Karthikeyan',
+  date: new Date().toLocaleDateString('en-GB', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+  }),
+};
+/**
+  * A - We open 'main.mustache'
+  * B - We ask Mustache to render our file with the data
+  * C - We create a README.md file with the generated output
+  */
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+generateReadMe();
