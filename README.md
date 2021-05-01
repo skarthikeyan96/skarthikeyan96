af68d1700de0eaf31d0f730b283f33c75b2c28c3
Karthikeyan <karthikeyan@bangthetable.com>
2021-05-02 00:28:42 +0530

    feat: added Latest blog post listing

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 <img src=https://user-images.githubusercontent.com/23126394/114269492-08f9fb00-9a25-11eb-89e0-a46912340a0b.png alt="banner" />
 
-My name is Karthikeyan and I am Software Engineer and today is Saturday, 1 May, 18:14 UTC.
+🕰️ Sunday, 2 May, 00:27 GMT+5:30
 
+Karthikeyan working as a Front End Engineer @Bang the table 
+ 
+ <ul>
+    <li id="654323"> 
+    <a href=https:&#x2F;&#x2F;dev.to&#x2F;imkarthikeyan&#x2F;1-getting-started-with-preact-preact-series-4ca> #1 Getting started with Preact - Preact Series  </a> 
+    <p> Reaction Count : 5 </p>
+    </li>
+    <li id="653521"> 
+    <a href=https:&#x2F;&#x2F;dev.to&#x2F;imkarthikeyan&#x2F;my-vs-code-setup-2ffa> My VS Code Setup  </a> 
+    <p> Reaction Count : 11 </p>
+    </li>
+    <li id="613729"> 
+    <a href=https:&#x2F;&#x2F;dev.to&#x2F;imkarthikeyan&#x2F;how-to-serve-an-asset-through-jsdelivr-cdn-from-github-4g61> How to serve an asset through jsdelivr CDN from Github.  </a> 
+    <p> Reaction Count : 4 </p>
+    </li>
+    <li id="580979"> 
+    <a href=https:&#x2F;&#x2F;dev.to&#x2F;imkarthikeyan&#x2F;100daysofgatsby-sprint-1-55h1> #100daysofGatsby - Sprint 1  </a> 
+    <p> Reaction Count : 0 </p>
+    </li>
+    <li id="556297"> 
+    <a href=https:&#x2F;&#x2F;dev.to&#x2F;imkarthikeyan&#x2F;javascript-closure-in-loops-ebi> Understanding JavaScript Closures in Loops  </a> 
+    <p> Reaction Count : 1 </p>
+    </li>
+</ul>
+
+<h4> Tools : </h4>
 <p>
   <img alt="React" src="https://img.shields.io/badge/-React-45b8d8?style=flat-square&logo=react&logoColor=white" />
   <img alt="Webpack" src="https://img.shields.io/badge/-Webpack-8DD6F9?style=flat-square&logo=webpack&logoColor=white" /> 
--- index.js
+++ index.js
@@ -1,34 +1,74 @@
 // index.js
-const Mustache = require('mustache');
-const fs = require('fs');
-const MUSTACHE_MAIN_DIR = './main.mustache';
+const Mustache = require("mustache");
+const fs = require("fs");
+const MUSTACHE_MAIN_DIR = "./main.mustache";
+const fetch = require("node-fetch");
+const orderBy = require("lodash/orderBy");
+const takeRight = require("lodash/takeRight")
+const reverse =require("lodash/reverse")
 /**
-  * DATA is the object that contains all
-  * the data to be provided to Mustache
-  * Notice the "name" and "date" property.
-*/
+ * DATA is the object that contains all
+ * the data to be provided to Mustache
+ * Notice the "name" and "date" property.
+ */
+const fetchBlog = async() => {
+  await fetch('https://dev.to/api/articles?username=imkarthikeyan')
+  .then((response) => { 
+      response.json().then((data) => {
+        setblogData(data);;
+      }).catch((err) => {
+          console.log(err);
+      }) 
+  });
+};
+
+
+
+
+const setblogData = (posts) => {
+  const Posts = orderBy(posts, ["published_timestamp"], ["asc"]).map((post) => {
+    return {
+      'id': post.id,
+      'title': post.title,
+      'url': post.url,
+      'reaction_count': post.public_reactions_count
+    };
+  });
+  DATA.blog = reverse(takeRight(Posts,5))
+};
+
 let DATA = {
-  banner: 'https://user-images.githubusercontent.com/23126394/114269492-08f9fb00-9a25-11eb-89e0-a46912340a0b.png',
-  name: 'Karthikeyan',
-  date: new Date().toLocaleDateString('en-GB', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    timeZoneName: 'short',
+  banner:
+    "https://user-images.githubusercontent.com/23126394/114269492-08f9fb00-9a25-11eb-89e0-a46912340a0b.png",
+  name: "Karthikeyan",
+  company: "Bang the table",
+  date: new Date().toLocaleDateString("en-GB", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZoneName: "short",
   }),
 };
+
 /**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+ * A - We open 'main.mustache'
+ * B - We ask Mustache to render our file with the data
+ * C - We create a README.md file with the generated output
+ */
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
+    fs.writeFileSync("README.md", output);
   });
 }
-generateReadMe();
+
+async function renderReadme(){
+  await fetchBlog();
+
+  await generateReadMe()
+}
+
+renderReadme()
